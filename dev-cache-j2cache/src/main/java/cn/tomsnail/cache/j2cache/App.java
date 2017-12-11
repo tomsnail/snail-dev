f5b042e91a75904820e39c8a6d07b198205a6325
yangsong608 <449711478@qq.com>
2017-12-11 13:53:46 +0800

    添加新组建：rap、j2cache

---
+++ dev-cache-j2cache/src/main/java/cn/tomsnail/cache/j2cache/App.java
@@ -0,0 +1,13 @@
+package cn.tomsnail.cache.j2cache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
