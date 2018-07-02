74358d6dadbf4c5483a91707a2b4bddb5a0ef9fb
tomsnail <yangsong608@qq.com>
2018-07-02 18:02:17 +0800

    修改版本为2.1.5-ts-SNAPSHOT

--- dev-task-elastic-job/src/main/java/cn/tomsnail/task/elastic/job/test/MyElasticJob.java
+++ dev-task-elastic-job/src/main/java/cn/tomsnail/task/elastic/job/test/MyElasticJob.java
@@ -1,17 +1,21 @@
 package cn.tomsnail.task.elastic.job.test;
 
 
-import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.ExecutionContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
 
 public class MyElasticJob implements SimpleJob{
 	
 
 	@Override
-	public void execute(ShardingContext context) {
-		System.out.println(context.getShardingItem());
-		System.out.println(context.getJobName());
-		System.out.println(context.getTaskId());
+	public void execute(ExecutionContext context) {
+		System.out.println(context.getShardingContext().getShardingItem());
+		System.out.println(context.getShardingContext().getJobName());
+		System.out.println(context.getShardingContext().getTaskId());
+		context.addMessage("execute now:"+System.currentTimeMillis());
+		if(System.currentTimeMillis()%2==1){
+			int i = 1/0;
+		}
 	}
 
 }
