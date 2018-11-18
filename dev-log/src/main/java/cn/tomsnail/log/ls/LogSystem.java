225714f02a32865c98965d614eeca1d4eca109ff
yangsong <449711478@qq.com>
2018-11-18 21:05:39 +0800

    update paramter name

--- dev-log/src/main/java/cn/tomsnail/log/ls/LogSystem.java
+++ dev-log/src/main/java/cn/tomsnail/log/ls/LogSystem.java
@@ -2,7 +2,7 @@ package cn.tomsnail.log.ls;
 
 public interface LogSystem {
 	
-	public void log(String context);
+	public void log(String message);
 	
 	public void log(String message, Object...objects );
 	
