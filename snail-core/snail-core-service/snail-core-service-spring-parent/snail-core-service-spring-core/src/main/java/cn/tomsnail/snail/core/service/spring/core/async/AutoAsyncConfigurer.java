d67b4a2a5e58c99c9481ec008dd591d1f883c87f
yangsong <449711478@qq.com>
2019-04-28 22:13:20 +0800

    添加spring 相关组件

---
+++ snail-core/snail-core-service/snail-core-service-spring-parent/snail-core-service-spring-core/src/main/java/cn/tomsnail/snail/core/service/spring/core/async/AutoAsyncConfigurer.java
@@ -0,0 +1,40 @@
+package cn.tomsnail.snail.core.service.spring.core.async;
+
+import cn.tomsnail.snail.core.util.configfile.ConfigHelp;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AutoAsyncConfigurer implements AsyncConfigurer {
+
+
+
+    public Executor getAsyncExecutor() {
+        // 定义线程池
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        // 核心线程数
+        taskExecutor.setCorePoolSize(Integer.parseInt(ConfigHelp.getInstance("config").getLocalConfig("spring.async.executor.core.size","10")));
+        // 线程池最大线程数
+        taskExecutor.setMaxPoolSize(Integer.parseInt(ConfigHelp.getInstance("config").getLocalConfig("spring.async.executor.max.size","30")));
+        // 线程队列最大线程数
+        taskExecutor.setQueueCapacity(Integer.parseInt(ConfigHelp.getInstance("config").getLocalConfig("spring.async.executor.queue.capacity","1000")));
+        // 初始化
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    /**
+     * The {@link AsyncUncaughtExceptionHandler} instance to be used
+     * when an exception is thrown during an asynchronous method execution
+     * with {@code void} return type.
+     */
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return null;
+    }
+}
